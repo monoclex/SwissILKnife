73876ca6a59e6dad2bd1cd16675294dc185c6dfd
SirJosh3917 <SirJosh3917@users.noreply.github.com>
2020-01-03 16:30:43 -0500

    bug in ValueType tests

--- src/SwissILKnife.Tests/Getters/ValueTypeTests.Magic.cs
+++ src/SwissILKnife.Tests/Getters/ValueTypeTests.Magic.cs
@@ -8,10 +8,10 @@ namespace SwissILKnife.Tests.Getters
 	public partial struct ValueType
 	{
 		private static PropertyInfo Property(string name, BindingFlags flags)
-			=> typeof(ReferenceType).GetProperty(name, flags);
+			=> typeof(ValueType).GetProperty(name, flags);
 
 		private static FieldInfo Field(string name, BindingFlags flags)
-			=> typeof(ReferenceType).GetField(name, flags);
+			=> typeof(ValueType).GetField(name, flags);
 	}
 
 	public partial class ValueTypeTests
